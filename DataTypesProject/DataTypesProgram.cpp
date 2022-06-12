c4cb554cc8120570d94000e668f2e43e16d83452
Mohammad Nauman <md.nauman@gmail.com>
2022-06-11 19:13:27 -0700

    printing std::cout and reading std::cin

--- DataTypesProject/DataTypesProgram.cpp
+++ DataTypesProject/DataTypesProgram.cpp
@@ -7,11 +7,11 @@
 
 #include <iostream>
 
-int main(){
+int main() {
 	/*int a;
-	int b;
-	int c, d;
-	double f;*/
+	 int b;
+	 int c, d;
+	 double f;*/
 
 	// define NOT initialize
 	int width;
@@ -34,7 +34,7 @@ int main(){
 	std::cout << c;
 
 	//4. more interesting, Brace initialization...initialize thru curly braces, used for better safety
-	int f{7};
+	int f { 7 };
 	std::cout << "\n";
 	std::cout << f;
 
@@ -42,32 +42,46 @@ int main(){
 	//direct initialization would work fine and at runtime value conversion would happen
 	int m(4.5);
 	std::cout << "\n";
-	std::cout << m;
+	std::cout << m << "\n";
 
 	//brace initialization won't work for incompatible data types
 	//int n{4.5}; //compiler would complain about it
 
 	// brace init in 3 flavors
-	int w{5};
-	int h = {6};
-	int d{};
+	int w { 5 };
+	int h = { 6 };
+	int d { };
 
 	// wrong...p is not initialized correctly
 	int p, q = 15;
-	std::cout << "\n";
 	std::cout << p; // wrong...prints 1 not 15
 
 	// correct...both vars are initialized correctly
 	int s = 5, t = 15;
 	std::cout << "\n";
-	std::cout << s; // correct...prints 5
+	std::cout << s << "\n"; // correct...prints 5
 
-	int x{5}, y{10};
-	std::cout << x + y; // correct...prints 5
+	int x { 5 }, y { 10 };
+	std::cout << x << " is smaller than " << y << std::endl; // correct...prints 5
+	//note: use \n instead of std::endl because the latter requires flush in addition to putting the cursor in next line
+
+	std::cout << "Hello " << "World!!!" << "\n"; // insertion operator << is used to concatenate multiple values
+	//use << (insertion operator to put the cursor to next line)
+
+	std::cout << "\n*******************************\n\n";
+
+	// read the user input
+	std::cout << "Enter the number: ";
+
+	// define the number
+	int xx { }; //1. as a good practice, we used brace initialization 2. we didn't initialize it with zero as the value won't be used immediately, instead it will be replaced with the value provided by the user
+
+	std::cin >> xx; // this capture the value from the keyboard and stores it into the variable xx
+
+	std::cout << "You entered the value --> " << xx << "\n";
 
 	// end of the program
 	return 0;
 
 }
-
 
