62409b33af92dff3ba1174813e8e9cfa2fcc39b0
Mohammad Nauman <md.nauman@gmail.com>
2022-06-12 08:43:59 -0700

    Done naming conventions

---
+++ CPPVariableProject/CPPVariableProgram.cpp
@@ -0,0 +1,42 @@
+/*
+ * CPPVariableProgram.cpp
+ *
+ *  Created on: Jun 11, 2022
+ *      Author: nauman
+ */
+
+#include <iostream>
+
+void doNothing(int&){
+
+}
+
+void varNamingConvention(){
+	// Identifier: name of a variable
+	// start with a lower case
+	// user-defined types (structs, classes, enumerations) can start with capital letters
+	// for multiword, use underscore or camelCase (preferred)
+	// Avoid naming start with underscore
+
+	std::cout << "\nHello World\n";
+
+	std::cout << "\nHello"
+			"World\n\n";
+
+	std::cout << 3 + 4
+		+ 5 + 6
+		* 2 * 3 << "\n";
+}
+
+
+int main(){
+
+	int x;
+	doNothing(x);
+	std::cout << x << "\n";
+	varNamingConvention();
+	return 0;
+
+}
+
+
