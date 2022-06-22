287c1aec813ce06ab493e10ceb17817441e40690
Mohammad Nauman <md.nauman@gmail.com>
2022-06-22 06:21:10 -0700

    implicit and explicit data conversion

--- ch4-DataTypes/Ch4-CharDataTypeProject/main.cpp
+++ ch4-DataTypes/Ch4-CharDataTypeProject/main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 void basicCharDataType();
 void inputChar();
+void print(double x);
+
 int main() {
-    inputChar();
+    //basicCharDataType();
+    //inputChar();
+    print(5.0);
     return 0;
 }
 
+void print(double x){
+    std::cout << x;
+    double y{3.0}; // c++ compiler drops zero in printing (only print non-zero fractions in float numbers)
+    std::cout << "The value of double y is: " << y << "\n";
+
+}
 void inputChar(){
     std::cout << "Please enter your choice (y/n): ";
     char ans{};
     std::cin >> ans;
     std::cout << "You entered --> " << ans << "\n";
+
+    std::cin >> ans;
+    std::cout << "You entered --> " << ans << "\n";
+
 }
 void basicCharDataType(){
     char ch1{5};
