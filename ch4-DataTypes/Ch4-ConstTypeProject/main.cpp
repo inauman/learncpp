0431961d2241a7fde56cb9ae65be20e3a5ede738
Mohammad Nauman <md.nauman@gmail.com>
2022-06-24 12:42:48 -0700

    ch4 - constant param value

--- ch4-DataTypes/Ch4-ConstTypeProject/main.cpp
+++ ch4-DataTypes/Ch4-ConstTypeProject/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+void printInt(const int x);
+void initConstant();
 
 int main() {
+    //initConstant();
+    printInt(5);
+    printInt(7);
+
+    return 0;
+}
+
+void printInt(const int x){
+    std::cout << x << "\n";
+
+    //x = 6; //won't compile. param can't change.
+}
+
+void initConstant(){
     std::cout << "Please enter your age: ";
     int age{};
     std::cin >> age;
@@ -12,6 +28,8 @@ int main() {
     age = 5;
 
     //constAge = age; //won't work. while a constant can be initialized with a variable, it can't be reassined.
+}
 
-    return 0;
+const int constReturnValue(){
+    return 5;
 }
