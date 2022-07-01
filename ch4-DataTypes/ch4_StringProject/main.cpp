c16ee4b9d0c5d286582dfb1ff13fdee66c1ef158
Mohammad Nauman <md.nauman@gmail.com>
2022-07-01 00:48:53 -0500

    ch4 string_view

--- ch4-DataTypes/ch4_StringProject/main.cpp
+++ ch4-DataTypes/ch4_StringProject/main.cpp
@@ -1,14 +1,17 @@
 
 #include <iostream>
 #include <string>
+#include <string_view>
 
 void readUserInput();
+void stringString();
+void stringString();
 
 int main() {
 
     readUserInput();
+    //stringString();
 
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
 
@@ -18,9 +21,25 @@ void readUserInput(){
     std::getline(std::cin >> std::ws, name);
 
     std::cout << "Please enter your age: ";
-    std::string age{};
-    std::getline(std::cin >> std::ws, age);
+    int age{};
+    std::cin >> age;
 
     std::cout << "Your name is \"" << name << "\" and your age is " << age << ".\n";
 
+    std::cout << "You have " << std::ssize(name) << " characters in your name." << "\n";
+    
+    std::cout << std::ssize(name) + age << "\n";
+
+}
+
+void stringString(){
+    using namespace std::literals;
+
+    std::cout << "foo\n"; //no suffix, c-style
+    std::cout << "goo\n"s; //s suffix is a standard c++ string literal
+    std::cout << "moo\n"sv; //sv string_view literal
+
+    //constexpr
+    //constexpr std::string_view name1{"Nauman"s};
+    //constexpr std::string_view name{ "Alex"s };
 }
