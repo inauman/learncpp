b89018c2010cb2ad98ee8f365dfb073e9f3339a6
Mohammad Nauman <md.nauman@gmail.com>
2022-07-04 21:13:55 -0500

    ch4 quiz 3 - done

--- ch4-DataTypes/ch4-QuizTimeProject/main.cpp
+++ ch4-DataTypes/ch4-QuizTimeProject/main.cpp
@@ -1,20 +1,60 @@
 #include <iostream>
 
-void readUserInput();
+void doQuiz3();
+double getDouble();
+char getOp();
+void printResult(double x, char operation, double y);
 
 int main() {
-    readUserInput();
+    doQuiz3();
     return 0;
 }
 
-void readUserInput(){
-    std::cout << "Please enter num1: ";
-    double num1{};
-    std::cin >> num1;
+void doQuiz3(){
 
-    std::cout << "Please enter num2: ";
-    double num2{};
-    std::cin >> num2;
+    // get the first number
+    double num1{getDouble()};
 
-    std::cout << "You have entered " << num1 << " and " << num2 <<".\n";
+    //get the second number
+    double num2{getDouble()};
+
+    //get the op
+    char op{getOp()};
+
+    // do the op and print results
+    printResult(num1, op, num2);
+}
+
+double getDouble(){
+    std::cout << "Please enter the number: ";
+    double num{};
+    std::cin >> num;
+
+    return num;
+}
+
+char getOp(){
+    std::cout << "Enter one of the following: +, -, *, or /: ";
+    char op{};
+    std::cin >> op;
+
+    return op;
+}
+
+void printResult(double num1, char op, double num2){
+    if (op == '+'){
+        std::cout << num1 << " " << op << " " << num2 << " = " << (num1 + num2);
+    }
+    else if (op == '-'){
+        std::cout << num1 << " " << op << " " << num2 << " = " << (num1 - num2);
+    }
+    else if (op == '*'){
+        std::cout << num1 << " " << op << " " << num2 << " = " << (num1 * num2);
+    }
+    else if (op == '/'){
+        std::cout << num1 << " " << op << " " << num2 << " = " << (num1 / num2);
+    }
+    else{
+        std::cout << "Sorry! You didn't entger a valid op. try again!!!";
+    }
 }
