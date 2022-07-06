d860d6fbd5f7fc2f1b686eb6a69bc75c9ee3e3e8
Mohammad Nauman <md.nauman@gmail.com>
2022-07-06 02:10:07 -0500

    ch5 exponents

--- ch4-DataTypes/ch5-OpsProject/main.cpp
+++ ch4-DataTypes/ch5-OpsProject/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <cmath>
+
 void doIntegerDiv();
 void doExponent();
+int64_t powint(int base, int exp);
+void quiz2();
+bool isEven(int num);
 
 int main() {
-    doIntegerDiv();
+    //doIntegerDiv();
+    //doExponent();
+    //std::cout << powint(7, 12);
+    quiz2();
     return 0;
 }
 
@@ -18,5 +26,38 @@ void doIntegerDiv(){
 }
 
 void doExponent(){
+    double x {std::pow(7, 12)};
+    std::cout << x << "\n";
+}
 
+int64_t powint(int base, int exp){
+    assert(exp >= 0 && "powint: exp parameter has negative value");
+
+    std::int64_t result{ 1 };
+    while(exp){
+        if (exp & 1){
+            result *= base;
+        }
+        exp >>= 1;
+        base *= base;
+    }
+    return result;
+}
+
+void quiz2(){
+    std::cout << "Please enter a number: ";
+    int num{};
+    std::cin >> num;
+
+    if (isEven(num)){
+        std::cout << "You entered an even number" << "\n";
+    }
+    else{
+        std::cout << "You entered an odd number" << "\n";
+    }
+
+}
+
+bool isEven(int num){
+   return (num % 2) == 0;
 }
