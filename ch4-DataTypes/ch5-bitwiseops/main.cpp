f30d665dbd65af7f2097220cd49ead3fdc02c2c1
Mohammad Nauman <md.nauman@gmail.com>
2022-07-17 19:19:12 -0700

    ch5: optional: bitwise ops n bit mask

--- ch4-DataTypes/ch5-bitwiseops/main.cpp
+++ ch4-DataTypes/ch5-bitwiseops/main.cpp
@@ -4,10 +4,37 @@ void basic();
 void bitwiseOps();
 std::bitset<4> rotl(std::bitset<4> bits);
 std::bitset<4> rotl2(std::bitset<4> bits);
+void rotateLeft();
+void bitmask();
+
 int main() {
     //basic();
     //bitwiseOps();
+    //rotateLeft();
+    bitmask();
 
+    return 0;
+}
+
+void bitmask(){
+    constexpr std::uint8_t mask0{ 0b0000'0001 }; //represents bit 0
+    constexpr std::uint8_t mask1{ 0b0000'0010 }; //represents bit 1
+    constexpr std::uint8_t mask2{ 0b0000'0100 }; //represents bit 2
+    constexpr std::uint8_t mask3{ 0b0000'1000 }; //represents bit 3
+    constexpr std::uint8_t mask4{ 0b0001'0000 }; //represents bit 4
+    constexpr std::uint8_t mask5{ 0b0010'0000 }; //represents bit 5
+    constexpr std::uint8_t mask6{ 0b0100'0000 }; //represents bit 6
+    constexpr std::uint8_t mask7{ 0b1000'0000 }; //represents bit 7
+
+    //flag
+    std::uint8_t flags{ 0b0000'0101 };
+    
+    //std::cout << (flags & mask2);
+    std::cout << "bit 0 is " << ((flags & mask0) ? "on\n" : "off\n");
+    std::cout << "bit 1 is " << ((flags & mask1) ? "on\n" : "off\n");
+
+}
+void rotateLeft(){
     std::bitset<4> bits1{ 0b0001 };
     std::cout << rotl(bits1) << '\n';
 
@@ -19,10 +46,7 @@ int main() {
 
 
     std::cout << rotl2(bits2) << '\n';
-
-    return 0;
 }
-
 // "rotl" stands for "rotate left"
 std::bitset<4> rotl(std::bitset<4> bits)
 {
