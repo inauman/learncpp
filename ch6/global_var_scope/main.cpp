16506f33ccaec0f6f225748a370e286429c8d4ab
Mohammad Nauman <md.nauman@gmail.com>
2022-07-22 06:25:00 -0700

    ch6: global varible, static scope & default initialization

--- ch6/global_var_scope/main.cpp
+++ ch6/global_var_scope/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 int g_x{}; //global variable
 
+const int g_y{}; // global constant
+constexpr int g_z{}; //global constant
+
 void doSomething(){
     std::cout << g_x << std::endl; //without initializer global variable are initialized with 0 by default but local variables are not initialized.
     g_x = 3;
