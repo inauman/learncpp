82e98130d9e816eb768b9f907c445db077194bc5
Mohammad Nauman <md.nauman@gmail.com>
2022-07-26 06:21:21 -0700

    ch6: internal and external linkage

--- ch6/identifier_linkage/a.cpp
+++ ch6/identifier_linkage/a.cpp
@@ -1,2 +1,4 @@
 int g_var1{13}; //non-constant external by default
 extern const int g_var2{19}; //extern make this internal constant var as externally linked
+extern constexpr int g_var3{27}; //while a constexpr can be declared global using extern, it can't be forward declare in another file
+//as the compiler won't be able to know what the value of this variable would be in another file
--- ch6/identifier_linkage/main.cpp
+++ ch6/identifier_linkage/main.cpp
@@ -5,11 +5,13 @@ extern int g_var1; //forward declaration of global var
 extern const int g_var2; //forward declaration of global var
 const int g_y{1}; //constant var are internal by default
 extern int g_z{3}; //constant var can be made external (truly global) by using the specifier extern
+//extern constexpr int g_var3; //can not forward declare a constexpr global variable
 
 int add(int x, int y);
 
 int main(){
     std::cout << add(3, 4) << std::endl;
     std::cout << g_var1 << " : " << g_var2 << std::endl;
+    std::cout << g_var1 << " : " << g_var2 << g_var3 << std::endl;
     return 0;
 }
