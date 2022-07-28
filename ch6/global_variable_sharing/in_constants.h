5cd386236006a72b7af5e62fcc62804f7ef5cf4a
Mohammad Nauman <md.nauman@gmail.com>
2022-07-28 06:55:56 -0700

    inline constants

---
+++ ch6/global_variable_sharing/in_constants.h
@@ -0,0 +1,12 @@
+//
+// Created by Mohammad Nauman on 7/28/22.
+//
+
+#ifndef CH6_IN_CONSTANTS_CPP_H
+#define CH6_IN_CONSTANTS_CPP_H
+namespace in_constants {
+    inline constexpr double in_pi{3.14159};
+    inline constexpr double in_avogadro{6.0221413e23};
+    inline constexpr double in_gravity{9.2};
+}
+#endif //CH6_IN_CONSTANTS_CPP_H
--- ch6/global_variable_sharing/main.cpp
+++ ch6/global_variable_sharing/main.cpp
@@ -1,12 +1,17 @@
 #include "constants.h"
+#include "in_constants.h"
 #include <iostream>
 
-int main(){
+int main() {
     std::cout << "Enter a radius: ";
+
     int radius{};
+
     std::cin >> radius;
 
     std::cout << "The circumference is: " << 2.0 * constants::pi * radius << std::endl;
+
+    std::cout << "The in_circumference is: " << 2.0 * in_constants::in_pi * radius << std::endl;
 
     return 0;
 }
