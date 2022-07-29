1de7cfc6aabb7ca1de8caa58dcaf29c7b5e6731b
Mohammad Nauman <md.nauman@gmail.com>
2022-07-29 11:13:16 -0700

    ch6 complete w/ quizzes

--- ch6/cmake-build-debug/cmake_install.cmake
+++ ch6/cmake-build-debug/cmake_install.cmake
@@ -77,6 +77,21 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/Users/nauman/Projects/learncpp/ch6/inline_namespaces/inline_namespaces_output/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/nauman/Projects/learncpp/ch6/quiz1/quiz1_output/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/nauman/Projects/learncpp/ch6/quiz2/quiz2_output/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/Users/nauman/Projects/learncpp/ch6/quiz3/quiz3_output/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
---
+++ ch6/quiz2/constants.h
@@ -0,0 +1,10 @@
+//
+// Created by Mohammad Nauman on 7/29/22.
+//
+
+#ifndef CH6_CONSTANTS_H
+#define CH6_CONSTANTS_H
+namespace constants{
+    inline constexpr int max_class_size = 35;
+}
+#endif //CH6_CONSTANTS_H
