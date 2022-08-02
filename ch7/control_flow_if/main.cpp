affdbbbd58344dd029b34a0cf4af9ae8e90c92d6
Mohammad Nauman <md.nauman@gmail.com>
2022-08-02 11:53:10 -0700

    ch7: switch stmt quiz

--- ch7/control_flow_if/main.cpp
+++ ch7/control_flow_if/main.cpp
@@ -1,24 +1,70 @@
 #include <iostream>
 
+void printDigitName(int x);
+int calculate(int op1, int op2, char op);
+
 int main() {
 
-    std::cout << "Enter 0 or 1: ";
+    //printDigitName(2);
+    std::cout << calculate(20, 5, '+') << "\n";
+    std::cout << calculate(20, 5, '-') << "\n";
+    std::cout << calculate(20, 5, '*') << "\n";
+    std::cout << calculate(20, 5, '/') << "\n";
+    std::cout << calculate(21, 5, '%') << "\n";
+    std::cout << calculate(27, 5, '@') << "\n";
 
-    int x{};
-    std::cin >> x;
 
-    if (x == 0){
-        std::cout << "You entered 0";
-    }
-    else{
-        std::cout << "You entered " << x;
-    }
-    int y{};
-
-    if(y=0)
-        ;   //empty or null statement...be careful...don't end the if stmt after the parenthesis.
-    std::cout << "\ndone....";
     return 0;
 }
 
+int calculate(int op1, int op2, char op){
 
+    switch (op) {
+        case '+': {
+            return op1 + op2;
+        }
+        case '-': {
+            return op1 - op2;
+        }
+        case '*': {
+            return op1 * op2;
+        }
+        case '/': {
+            return op1 / op2;
+        }
+        case '%': {
+            return op1 % op2;
+        }
+        default:{
+            std::cerr << "calculate(): Unhandled case\n";
+            return 0;
+        }
+
+
+    }
+    return 0;
+}
+void printDigitName(int x) {
+    switch (x) {
+        int a;
+        case 1: {
+            std::cout << "\nOne";
+            return;
+        }
+        case 2: {
+            std::cout << "\nTwo";
+            [[fallthrough]];
+        }
+        case 3: {
+            std::cout << "\nThree";
+            break;
+        }
+        default: {
+            std::cout << "Default";
+            return;
+        }
+    }
+
+    // execution continues here
+    std::cout << " \nAh-Ah-Ah!";
+}
